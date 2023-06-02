3457b0dc159434bbe06f86a194104853f932d87a
汪子涵 <17344612806@163.com>
2023-06-02 18:16:49 +0800

    样式框架搭建：iconfont引入测试，aside底部按钮增加

--- src/components/aside/index.vue
+++ src/components/aside/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div :class="'tem-aside ' + (collapse ? 'collapse' : '')">
-    <el-button @click="handleCollapse()">切换</el-button>
+    <el-button @click="handleCollapse">切换</el-button>
+    <div class="footer" @click="handleCollapse">
+      <span class="iconfont icon-collapse"></span>
+    </div>
   </div>
 </template>
 
@@ -8,4 +11,15 @@
 import { collapse, handleCollapse } from "@/components/aside/index.ts";
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.footer {
+  transition: 200ms;
+  width: var(--aside-width);
+  height: 30px;
+  font-size: 20px;
+  text-align: center;
+  border-top: 1px solid rgba(128, 128, 128, 0.2);
+  position: absolute;
+  bottom: 0;
+}
+</style>
